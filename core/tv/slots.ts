0a532a584e969c6336418d995970c6d2537960df
thebigrick <riccardo.tempesta@bigcommerce.com>
2024-11-29 16:17:48 +0100

    WIP

--- core/tv/slots.ts
+++ core/tv/slots.ts
@@ -7,16 +7,13 @@ export type SlotsToClasses<S extends string> = {
 export const getSlots = <Slots extends string>(
   slotsFn: () => Record<Slots, (props: { class?: ClassValue }) => string>,
   classNames: SlotsToClasses<Slots> | undefined,
-): Record<keyof SlotsToClasses<Slots>, () => string> => {
+): Record<Slots, () => string> => {
   const slots = slotsFn();
 
-  return Object.keys(slots).reduce<Record<keyof SlotsToClasses<Slots>, () => string>>(
-    (acc, k: string) => {
-      return {
-        ...acc,
-        [k]: slots[k]({ class: classNames?.[key] }),
-      };
-    },
-    {},
-  );
+  return Object.keys(slots).reduce<Record<Slots, () => string>>((acc, k: string) => {
+    return {
+      ...acc,
+      [k]: slots[k]({ class: classNames?.[key] }),
+    };
+  }, {});
 };
